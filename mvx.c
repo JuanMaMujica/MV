1fd72773c239dc2600bf908aff8525920d2b7ddf
JuanMaMujica <50682533+JuanMaMujica@users.noreply.github.com>
2022-04-16 13:07:35 -0300

    Ejecucion
    Guarda los valores de los dos operandos

--- mvx.c
+++ mvx.c
@@ -15,6 +15,9 @@ typedef struct {
     __int32 cod;
 } elementosMnemonicos;
 
+TRegistros Registros[16];
+__int32 Memoria[cantidadMR]={0};
+
 //--------------------------------------Prototipos---------------------------------------------------
 void InicializaRegistros(TRegistros Registros[]);
 void cargaMnemonicos(elementosMnemonicos mnemonicos[]);
@@ -24,8 +27,7 @@ void cargaMnemonicos(elementosMnemonicos mnemonicos[]);
 //-----------------------------------------MV----------------------------------------------------------
 
 void main(int arg,char *args[]){
-    TRegistros Registros[16];
-    __int32 Memoria[cantidadMR]={0};
+    
     elementosMnemonicos Mnemonicos[25];
     __int32 Header[6]={0};
     FILE *archI;
@@ -126,9 +128,9 @@ void leeInstruccion(TRegistros Registros[],__int32 Memoria[]){
     int cantidadOperandos=0;
     int ip=Registros[5].ValorRegistro;
     int ds=Registros[0].ValorRegistro;
-    __int32 instruccion,mnemonico,tipoOp1,tipoOp2,op1,op2;
-    void (fun[])(__int32, __int32 ) = {mov, add, sub,swap,mul,DIV,cmp,shl,shr,and,or,xor};
-    void (fun2[])(__int32 *) = {sys,jmp, jz,jp,JN,jnz,jnp,jnn,ldl,ldh,rnd,not};
+    __int32 instruccion,mnemonico,tipoOp1,tipoOp2,op1,op2,valorOp1,valorOp2;
+    void (*fun[])(__int32 *, __int32 *) = {mov, add, sub,swap,mul,DIV,cmp,shl,shr,and,or,xor};
+    void (*fun2[])(__int32 *) = {sys,jmp, jz,jp,JN,jnz,jnp,jnn,ldl,ldh,rnd,not};
 
     while (Registros[5].ValorRegistro>=0 && Registros[5].ValorRegistro<ds){
         instruccion = Memoria[ip];
@@ -139,17 +141,45 @@ void leeInstruccion(TRegistros Registros[],__int32 Memoria[]){
             tipoOp2 = (instruccion >> 24) & 0X3;
             op1 = (instruccion >> 12) & 0XFFF;
             op2 = instruccion & 0XFFF;
+            valorOp1 = decodificaOperando(op1,tipoOp1);
+            valorOp2 = decodificaOperando(op2,tipoOp2);
+            valorOp2=valorOp2<<20;  //propaga el bit de signo de un operando inmediato negativo, de ser positivo no importa el corrimiento queda igual
+            valorOp2=valorOp2>>20;
 
+            (*fun[mnemonico])(&valorOp1,&valorOp2); //llama a la instruccion correspondiente dependiendo del mnemonico
+            if(mnemonico!=0X6){ // alamcena los valores calculados anteriormente en los registros o memoria correspondiente menos en el cmp 
+                alamacenaRM(valorOp1,tipoOp1,op1);
+                alamacenaRM(valorOp2,tipoOp2,op2);
+            }
 
-            (*fun[mnemonico])(op1,op2)
+            if(mnemonico != 0X0 && mnemonico != 0X3 && mnemonico !=0X6 ){ // cambia el valor de CC seguun el resultado que se calcule
+                cambiaCC(valorOp1);
+            }
+
         } else if(cantidadOperandos == 1){
             tipoOp1 = (instruccion>>22) & 0X3;
             op1 = instruccion & 0XFFFF; 
+            
+
+
+        } else {
+            stop();
         }
 
     }
     
 
+}
+
+void cambiaCC(__int32 valorOp1){
+   
+   if(valorOp1 == 0){
+        Registros[8].ValorRegistro=0X1;
+    } else if(valorOp1 < 0){
+        Registros[8].ValorRegistro=0X80000000;
+    } else {
+        Registros[8].ValorRegistro=0X0;
+    }
 }
 
 __int8 recorreMnemonicos(elementosMnemonicos mnemonicos[],__int32 mnemonico)
@@ -187,15 +217,27 @@ __int32 leeMnemonico(__int32 instruccion,int *cantidadOperandos){
     return mnemonico;
 }
 
-__int32 decodificaOperando(__int32 op, __int32 tipoOp,TRegistros Registro[]){
+__int32 decodificaOperando(__int32 op, __int32 tipoOp){
 
-    if(tipoOp == 0){ //es inmediato
-        return op;
-    } else if(tipoOp == 1){//es registro 
-        op = Registro[op].ValorRegistro;
-    } else {}
+    if(tipoOp == 1){//es registro 
+        op = Registros[op].ValorRegistro;
+    } else if(tipoOp == 2) {    //es directo
+        op = Memoria[op+Registros[0].ValorRegistro];
+    }
+    return op; // si es inmediato lo devuelve igual
 }
 
+void alamacenaRM(__int32 valorOp, __int32 tipoOp, __int32 op){
+    if(tipoOp == 1){
+        Registros[op].ValorRegistro = valorOp;
+    } else if(tipoOp == 2){
+        Memoria[op+Registros[0].ValorRegistro] = valorOp;
+    }
+}
+
+
+
+//-------------------------------------------OPERACIONES--------------------------------------------------
 void mov(__int32 *a, __int32 *b){
     *a = *b;
 }
@@ -251,3 +293,63 @@ void shl(__int32 *a, __int32 *b){
 void shr(__int32 *a, __int32 *b){
     *a = *a >> *b;
 } 
+
+void jmp(__int32 *a){
+    Registros[5].ValorRegistro = *a;
+}
+
+void jz(__int32 *a){
+    if((Registros[8].ValorRegistro & 0x00000001) == 0x00000001){
+        Registros[5].ValorRegistro = *a;
+    }
+}
+
+void jp(__int32 *a){
+    if(Registros[8].ValorRegistro == 0){ //Condición vieja: (Registros[8].ValorRegistro & 0x80000000) == 0x00000000)
+        Registros[5].ValorRegistro = *a;
+    }
+}
+
+void JN(__int32 *a){
+    if((Registros[8].ValorRegistro & 0x80000000) == 0x80000000){
+        Registros[5].ValorRegistro = *a;
+    }
+}
+
+void jnz(__int32 *a){
+    if((Registros[8].ValorRegistro & 0x00000001) == 0x00000000){
+        Registros[5].ValorRegistro = *a;
+    }
+}
+
+void jnp(__int32 *a){
+    if(((Registros[8].ValorRegistro & 0x80000000) == 0x80000000) || (Registros[8].ValorRegistro == 1)){ //Si es negativo Ó 0.
+        Registros[5].ValorRegistro = *a;
+    }
+}
+
+void jnn(__int32 *a){
+    if((Registros[8].ValorRegistro & 0x80000000) == 0x00000000){
+        Registros[5].ValorRegistro = *a;
+    }
+}
+
+void ldl(__int32 *a){
+    *a = *a & 0x0000FFFF;
+    *a += (Registros[9].ValorRegistro & 0xFFFF0000); //Le suma los 2 BYTES más significativos del AC.
+    Registros[9].ValorRegistro = *a;
+}
+
+void ldh(__int32 *a){
+    *a = *a << 16;
+    *a += (Registros[9].ValorRegistro & 0x0000FFFF); //Le suma los 2 BYTES menos significativos del AC.
+    Registros[9].ValorRegistro = *a;
+}
+
+void rnd(__int32 *a){
+    Registros[9].ValorRegistro = rand() % (*a + 1);
+}
+
+void not(__int32 *a){
+    *a = ~(*a);
+}
